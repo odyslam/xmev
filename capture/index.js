1a619595e1915a833f3e50af0d8ad75d29b2bca4
odyslam.eth <odyslam@gmail.com>
2022-03-28 18:37:46 +0300

    fix: correct polygon provider, add lag (#4)

--- capture/index.js
+++ capture/index.js
@@ -18,43 +18,39 @@ async function main() {
   const bucket = 'xmev'
 
   const ethereumProvider = new ethers.providers.AlchemyProvider(null, ethereumKey);
-  const polygonProvider = new ethers.providers.AlchemyProvider(null, polygonKey);
+  const polygonProvider = new ethers.providers.AlchemyProvider('matic', polygonKey);
 
   const client = new InfluxDB({url: 'https://europe-west1-1.gcp.cloud2.influxdata.com', token: token})
   const writeApi = client.getWriteApi(org, bucket)
   writeApi.useDefaultTags({host: 'xmev-capture'})
 
-  ethereumProvider.on('block', (block) => {
+  ethereumProvider.on('block', async (block) => {
     lastEthBlockTime = Date.now();
     lastEthBlock = block;
-    const ethBlock = new Point('ethereum_timestamps')
-      .tag("block", block)
-      .intField('timestamp', lastEthBlockTime)
+    let b = await ethereumProvider.getBlock(block);
+    let lagData = Math.floor(lastEthBlockTime / 1000) - b.timestamp;
+    const lag = new Point('ethereum_lag').tag('timestamp', lastEthBlockTime).floatField("lag", lagData);
     let timeDelta = (lastEthBlockTime - lastPolygonBlockTime);
     const delta_blocks = new Point('delta_blocks')
-      .tag("eth_block", block)
-      .tag("pol_block", lastPolBlock)
       .tag("timestamp", lastEthBlockTime)
       .floatField("delta", blockDelta)
     if (!isNaN(timeDelta)) {
       const delta_time = new Point('delta_block_time')
-        .tag("eth_block", block)
-        .tag("pol_block", lastPolBlock)
         .tag("timestamp", lastEthBlockTime)
         .floatField("delta", timeDelta)
       if (transmit == "TRUE") {
-        writeApi.writePoints([ethBlock, delta_time, delta_blocks]);
+        writeApi.writePoints([delta_time, delta_blocks, lag]);
       }
     } else {
       if (transmit == "TRUE") {
-        writeApi.writePoints([ethBlock, delta_blocks]);
+        writeApi.writePoints([delta_blocks, lag]);
       }
     }
     console.log("ETHEREUM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     console.log(`New Eth Block: ${block}`);
     console.log(`Time delta: ${timeDelta}`);
     console.log("Writing point to InfluxDB");
-    console.log(ethBlock);
+    console.log(lag);
     if (!isNaN(timeDelta)) {
       console.log(timeDelta);
     }
@@ -63,21 +59,23 @@ async function main() {
     blockDelta = 0;
   });
 
-  polygonProvider.on('block', (block) => {
+  polygonProvider.on('block', async (block) => {
     lastPolygonBlockTime = Date.now();
     lastPolBlock = block;
     blockDelta += 1;
+    let b = await polygonProvider.getBlock(block);
+    let lagData = Math.floor(lastPolygonBlockTime / 1000) - b.timestamp;
+    const lag = new Point('polygon_lag').tag('timestamp', lastPolygonBlockTime).floatField("lag", lagData);
     const polBlock = new Point("polygon_timestamps")
       .tag("block", block)
       .intField('timestamp', lastPolygonBlockTime)
-    if (transmit == "TRUE") {
-      writeApi.writePoint(polBlock);
-    }
+    writeApi.writePoints([lag]);
     console.log("POLYGON ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     console.log(`New Polygon Block: ${block}`);
     console.log(`Block delta: ${blockDelta}`);
     console.log("Writing point to InfluxDB");
     console.log(polBlock);
+    console.log(lag);
     console.log("POLYGON ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
   });
   if (transmit == "TRUE") {
@@ -98,3 +96,4 @@ main()
     console.error(error);
     process.exit(1);
   });
+
